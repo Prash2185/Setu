2b20ec23dbe0cfa408d6a6ea2997f3927210b012
Prasanna Dhotarkar <prasannadhotarkar@gmail.com>
2026-05-30 00:51:12 +0530

    Handle POST /register: create local user, encode password

--- src/main/java/com/setu/cloudbilling/RegistrationController.java
+++ src/main/java/com/setu/cloudbilling/RegistrationController.java
@@ -1,14 +1,39 @@
 package com.setu.cloudbilling;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class RegistrationController {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private org.springframework.security.crypto.password.PasswordEncoder passwordEncoder;
+
     @GetMapping("/register")
     public ModelAndView showRegister() {
         return new ModelAndView("register");
+    }
+
+    @PostMapping("/register")
+    public String processRegister(@RequestParam String name,
+                                  @RequestParam String email,
+                                  @RequestParam String password) {
+        if (userRepository.findByEmail(email).isPresent()) {
+            return "redirect:/register?error=exists";
+        }
+        User user = new User();
+        user.setName(name);
+        user.setEmail(email);
+        user.setPassword(passwordEncoder.encode(password));
+        user.setProvider(AuthProvider.LOCAL);
+        userRepository.save(user);
+        return "redirect:/login?registered";
     }
 }
